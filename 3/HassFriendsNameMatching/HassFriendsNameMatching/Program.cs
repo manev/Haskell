96071bf74d59dcb28ce45dedcf898c9cc56e529f
manev <kokomanev84@gmail.com>
2015-06-13 09:53:03 +0300

    added solution for task 3

---
+++ 3/HassFriendsNameMatching/HassFriendsNameMatching/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HassFriendsNameMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter");
+
+            var nameMatcher = new NameMatcher();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "quit")
+            {
+                nameMatcher.CalculateChangeOfSuccess(input);
+            }
+        }
+    }
+}
---
+++ 3/HassFriendsNameMatching/HassFriendsNameMatchingTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HassFriendsNameMatching;
+
+namespace HassFriendsNameMatchingTests
+{
+    [TestClass]
+    public class NameMatcherTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var nameMatcher = new NameMatcher();
+        }
+    }
+}
